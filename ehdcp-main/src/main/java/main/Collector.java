02272a6610d94d003f8185af3f82f7daf459dc83
David Fan <David Fan>
2016-09-17 20:31:10 -0400

    Upload data to influxDB

--- ehdcp-main/src/main/java/main/Collector.java
+++ ehdcp-main/src/main/java/main/Collector.java
@@ -4,13 +4,24 @@ import adc.SpiADC;
 import com.pi4j.io.spi.SpiChannel;
 import com.pi4j.io.spi.SpiDevice;
 import com.pi4j.io.spi.SpiFactory;
+import org.influxdb.InfluxDB;
+import org.influxdb.InfluxDBFactory;
+import org.influxdb.dto.Point;
+
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 public class Collector {
 
     private SpiDevice spi;
     private SpiADC adc;
     private static final int INTERVAL = 1000;
+
+    private String serverIP;
+    private String dbName;
+    private InfluxDB influxDB;
 
     public Collector() {
         try {
@@ -36,6 +47,7 @@ public class Collector {
         while(true) {
             try {
                 double value[] = adc.readAll();
+                writeInfluxDB(value);
                 for (int i = 0; i < 16; i++) {
                     System.out.print(String.format(" | %1.3f", value[i]));
                 }
@@ -51,8 +63,33 @@ public class Collector {
         }
     }
 
+    public void setServer(String serverIP) {
+        this.serverIP = serverIP;
+        influxDB = InfluxDBFactory.connect("http://" + serverIP +":8086", "root", "root");
+    }
+
+    public void writeInfluxDB(final double[] data) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Map<String, Object> fields = new HashMap<String, Object>();
+                for (int i = 0; i< data.length; i++) {
+                    fields.put("ch" + i, data[i]);
+                }
+                Point point = Point.measurement("ad")
+                        .time(System.currentTimeMillis(), TimeUnit.MILLISECONDS)
+                        .fields(fields)
+                        .build();
+
+                influxDB.write("collector", "autogen", point);
+            }
+        }).start();
+
+    }
+
     public static void main(String[] args) {
         Collector c = new Collector();
+        c.setServer("128.143.24.101");
         c.run();
     }
 }
